fa2758bd207332ae02dae5d82a8adc3e95c56677
cornellvel1 <61205299+viola1963@users.noreply.github.com>
2024-05-10 17:32:34 -0400

    Not much changes. Added more comments to clarify code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,7 +14,9 @@ public class PlayerController : MonoBehaviour
     public GameObject leftController, rightController;
     public HapticController hapticLeft, hapticRight;
     List<Vector2Int> path;
- 
+    public GameObject camera;
+    public Vector3 cameraDir;
+
 
     void Start()
     {
@@ -29,6 +31,7 @@ public class PlayerController : MonoBehaviour
         /*  How Movement Works: Players Presses WASD which collects "Horizontal" and "Vertical" axis information, so that player can rotate 
             Then Player presses space bar to actually move forward.
         */
+        camera = GameObject.FindWithTag("MainCamera");
         leftController = GameObject.FindWithTag("LeftController");
         rightController = GameObject.FindWithTag("RightController");
         if (leftController != null && rightController != null)
@@ -36,7 +39,7 @@ public class PlayerController : MonoBehaviour
             hapticLeft = leftController.GetComponent<HapticController>();
             hapticRight = rightController.GetComponent<HapticController>();
         }
- 
+        cameraDir = camera.transform.position;
         CheckAndGuidePath();
     }
 
@@ -44,12 +47,12 @@ public class PlayerController : MonoBehaviour
     void CheckAndGuidePath()
     {
         /* Compares the path the player is on to the actual optimal path and recommends adjustments to player direction.*/
-
+        //Vector2Int currentDir = new Vector2Int(Mathf.RoundToInt(cameraDir.x), Mathf.RoundToInt(cameraDir.z));
         Vector2Int currentDir = new Vector2Int(Mathf.RoundToInt(transform.forward.x), Mathf.RoundToInt(transform.forward.z)); // discretizes the current player position
         Vector2Int currentPos = new Vector2Int(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.z)); // discretizes the current player position
 
         // if they go off track
-        /*if (!path.Contains(currentPos))
+        if (!path.Contains(currentPos))
         {
             Vector2Int nearestPos = FindNearestPathPoint(currentPos);
             Vector2Int differenceVector = nearestPos - currentPos;
@@ -62,7 +65,7 @@ public class PlayerController : MonoBehaviour
             // find angle in Betweeen two vectors
             float angleBetween = findAngle(a, b);
             ProvideDirection(angleBetween, false);
-        }*/
+        }
 
         // if player is on track
         if (path.Contains(currentPos))
@@ -133,19 +136,22 @@ public class PlayerController : MonoBehaviour
             duration = .5f;
             frequency = .9f;
         }
+        /*0.7853981f*/
          
-        if (angleBetween > 0.7853981f)
+        if (angleBetween > 0)
         {
-            hapticLeft?.SendHaptics(amplitude, duration, frequency);
+            // we hard coded this value because the left controller was feeling a bit weaker, so we gave it a higher amplitude
+            hapticLeft?.SendHaptics(.4f, duration, frequency);
         }
-        else if (angleBetween < -0.7853981f)
+        else if (angleBetween < 0)
         {
             hapticRight?.SendHaptics(amplitude, duration, frequency);
         }
-        else if (angleBetween < 0.7853981f && angleBetween > -0.7853981f)
+        else if (angleBetween== 0) /*angleBetween >= -0.7853981f && angleBetween <= 0.7853981f*/
         {
-            hapticLeft?.SendHaptics(amplitude, duration, frequency);
-            hapticRight?.SendHaptics(amplitude, duration, frequency);
+            // Uncomment this code to activate CONSTANT FEEDBACK MECHANISM. When commented this GPS MECHANISM
+           /* hapticLeft?.SendHaptics(.4f, duration, frequency);
+            hapticRight?.SendHaptics(amplitude, duration, frequency);*/
         }
 
     }
